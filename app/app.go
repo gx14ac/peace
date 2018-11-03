ebc24cfaf438c7cd027f58e180b2a073bed84be3
Okumura Shintarou <flekystyley@ezweb.ne.jp>
2018-11-03 22:10:28 +0900

    Create app Directory

---
+++ app/app.go
@@ -0,0 +1,28 @@
+package app
+
+import (
+	"github.com/OkumuraShintarou/peace/config"
+	"github.com/jinzhu/gorm"
+)
+
+type App struct {
+	dbm    *gorm.DB
+	config config.Config
+}
+
+var _app *App
+
+func Init(dbm *gorm.DB, cfg config.Config) {
+	_app = &App{
+		dbm:    dbm,
+		config: cfg,
+	}
+}
+
+func IsPrd() bool {
+	return _app.config.AppEnv == "prd"
+}
+
+func IsDev() bool {
+	return _app.config.AppEnv == "dev"
+}
