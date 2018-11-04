c04746712b4974442a9bde2e00cefb6f7fc4a2b2
Okumura Shintarou <flekystyley@ezweb.ne.jp>
2018-11-04 18:56:35 +0900

    a

---
+++ resources/strings/string.go
@@ -0,0 +1,34 @@
+package strings
+
+import "fmt"
+
+func Peace() {
+	s := `
+	/$$$$$$$$                  /$$     /$$ /$$          
+	| $$_____/                 | $$     \ $|__/          
+	| $$    /$$   /$$  /$$$$$$$| $$   /$$\/ /$$ /$$$$$$$ 
+	| $$$$$| $$  | $$ /$$_____/| $$  /$$/  | $$| $$__  $$
+	| $$__/| $$  | $$| $$      | $$$$$$/   | $$| $$  \ $$
+	| $$   | $$  | $$| $$      | $$_  $$   | $$| $$  | $$
+	| $$   |  $$$$$$/|  $$$$$$$| $$ \  $$  | $$| $$  | $$
+	|__/    \______/  \_______/|__/  \__/  |__/|__/  |__/
+
+	PPPPPPPPPPPPPPPPP   EEEEEEEEEEEEEEEEEEEEEE               AAA                  CCCCCCCCCCCCCEEEEEEEEEEEEEEEEEEEEEE
+	P::::::::::::::::P  E::::::::::::::::::::E              A:::A              CCC::::::::::::CE::::::::::::::::::::E
+	P::::::PPPPPP:::::P E::::::::::::::::::::E             A:::::A           CC:::::::::::::::CE::::::::::::::::::::E
+	PP:::::P     P:::::PEE::::::EEEEEEEEE::::E            A:::::::A         C:::::CCCCCCCC::::CEE::::::EEEEEEEEE::::E
+	  P::::P     P:::::P  E:::::E       EEEEEE           A:::::::::A       C:::::C       CCCCCC  E:::::E       EEEEEE
+	  P::::P     P:::::P  E:::::E                       A:::::A:::::A     C:::::C                E:::::E             
+	  P::::PPPPPP:::::P   E::::::EEEEEEEEEE            A:::::A A:::::A    C:::::C                E::::::EEEEEEEEEE   
+	  P:::::::::::::PP    E:::::::::::::::E           A:::::A   A:::::A   C:::::C                E:::::::::::::::E   
+	  P::::PPPPPPPPP      E:::::::::::::::E          A:::::A     A:::::A  C:::::C                E:::::::::::::::E   
+	  P::::P              E::::::EEEEEEEEEE         A:::::AAAAAAAAA:::::A C:::::C                E::::::EEEEEEEEEE   
+	  P::::P              E:::::E                  A:::::::::::::::::::::AC:::::C                E:::::E             
+	  P::::P              E:::::E       EEEEEE    A:::::AAAAAAAAAAAAA:::::AC:::::C       CCCCCC  E:::::E       EEEEEE
+	PP::::::PP          EE::::::EEEEEEEE:::::E   A:::::A             A:::::AC:::::CCCCCCCC::::CEE::::::EEEEEEEE:::::E
+	P::::::::P          E::::::::::::::::::::E  A:::::A               A:::::ACC:::::::::::::::CE::::::::::::::::::::E
+	P::::::::P          E::::::::::::::::::::E A:::::A                 A:::::A CCC::::::::::::CE::::::::::::::::::::E
+	PPPPPPPPPP          EEEEEEEEEEEEEEEEEEEEEEAAAAAAA                   AAAAAAA   CCCCCCCCCCCCCEEEEEEEEEEEEEEEEEEEEEE
+	`
+	fmt.Println(s)
+}
