3d2c24dc02ca6fa2e7ab691b6b9a56cc60b69e97
Okumura Shintarou <flekystyley@ezweb.ne.jp>
2018-11-12 21:17:30 +0900

    Fix Session.go

--- handler/session.go
+++ handler/session.go
@@ -1,8 +1,21 @@
 package handler
 
 import (
-	"net/http"
-	"time"
-
-	"github.com/gin-gonic/gin"
+	"github.com/OkumuraShintarou/peace/app"
+	"github.com/OkumuraShintarou/peace/apperr"
+	"github.com/OkumuraShintarou/peace/model"
+	"github.com/OkumuraShintarou/peace/service"
+	"github.com/OkumuraShintarou/peace/util"
 )
+
+func SessionSignUp(cc *util.CustomContext) {
+	var param model.SignUpParam
+
+	if bindErr := cc.BindJSON(&param); bindErr != nil {
+		err := apperr.NewError(apperr.RequestError, apperr.Info, bindErr.Error())
+		cc.AbortWithError(err)
+		return
+	}
+
+	userSvc := service.NewUser(app.DBM())
+}
