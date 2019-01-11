508d1381fd2d99c64071333d6dc4524ba69029a5
shinta <flekystyley@ezweb.ne.jp>
2019-01-12 00:19:34 +0900

    Fix main.go

--- main.go
+++ main.go
@@ -4,26 +4,25 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/OkumuraShintarou/peace/router"
 	"github.com/OkumuraShintarou/peace/app"
 	"github.com/OkumuraShintarou/peace/config"
 	"github.com/OkumuraShintarou/peace/db"
 	resource "github.com/OkumuraShintarou/peace/resources/strings"
+	"github.com/OkumuraShintarou/peace/router"
 	_ "github.com/go-sql-driver/mysql"
 )
-
-func main() {
-	cfg := config.New()
-	dbm := db.MustNewDB(cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPass, cfg.DBName)
-	app.Init(dbm, cfg)
-
-	db.Migrate(dbm)
-	r := router.New()
-	r.Run(":" + cfg.AppPort)
-}
 
 func init() {
 	resource.Peace()
 	fmt.Println("Set Time Zone...", time.UTC)
 	time.Local = time.UTC
+}
+
+func main() {
+	cfg := config.New()
+	dbm := db.MustNewDB(cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPass, cfg.DBName)
+	app.Init(dbm, cfg)
+	db.Migrate(dbm)
+	r := router.New()
+	r.Run(":" + cfg.AppPort)
 }
